ab6ff42afe9dc1e7d519993c7d6766ec9b55e3d4
sakaiyuto <yutorobo1202@gmail.com>
2025-02-01 12:50:47 +0900

    aiueod

---
+++ self-introduction.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# タイトル
+st.title("自己紹介")
+
+# 自己紹介の内容
+st.write("名前: 酒井優音")
+st.write("年齢: 12歳")
+st.write("趣味: ゲーム")
+st.write("好きな食べ物: 米")
+st.write("将来の夢: まだ決まっていない")
+
+# もし他に追加したいことがあれば、ここに書いてね！
