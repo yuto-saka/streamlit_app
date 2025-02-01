b8c90d3bf98e4a69b26ccbaa7851a0fd6158f306
sakaiyuto <yutorobo1202@gmail.com>
2025-02-01 12:57:08 +0900

    aiueod

--- self-introduction.py
+++ self-introduction.py
@@ -1,13 +1,21 @@
 import streamlit as st
 
-# タイトル
-st.title("自己紹介")
+# パスワードの設定
+password = st.text_input("パスワードを入力してください:", type="password")
 
-# 自己紹介の内容
-st.write("名前: 酒井優音")
-st.write("年齢: 12歳")
-st.write("趣味: ゲーム")
-st.write("好きな食べ物: 米")
-st.write("将来の夢: まだ決まっていない")
+# 正しいパスワード
+correct_password = "知るか"  # ここに自分が決めたパスワードを入れてね
 
-# もし他に追加したいことがあれば、ここに書いてね！
+# パスワードが正しいかチェック
+if password == correct_password:
+    # タイトル
+    st.title("自己紹介")
+
+    # 自己紹介の内容
+    st.write("名前: 酒井優音")
+    st.write("年齢: 12歳")
+    st.write("趣味: ゲーム")
+    st.write("好きな食べ物: 米")
+    st.write("将来の夢: まだ決まっていない")
+else:
+    st.write("パスワードが間違っています。再度入力してください。")
